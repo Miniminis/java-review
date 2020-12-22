884c1124c684713549b85600f3368ca8ed95004a
miniminis <minhee4735@gmail.com>
2020-12-23 08:08:19 +0900

    [Chapter11] ArrayList, LinkedList, Vector 복습

--- Chapter11/src/com/javareview/Main.java
+++ Chapter11/src/com/javareview/Main.java
@@ -1,10 +1,12 @@
 package com.javareview;
 
+import com.javareview.collection.LinkedListTest;
 import com.javareview.generic.GenericTest;
 
 public class Main {
 
     public static void main(String[] args) {
-        GenericTest.run();
+//        GenericTest.run();
+        LinkedListTest.run();
     }
 }
---
+++ Chapter11/src/com/javareview/collection/LinkedListTest.java
@@ -0,0 +1,29 @@
+package com.javareview.collection;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+
+    public static void run() {
+
+        LinkedList<String> strs = new LinkedList<>();
+        strs.add("A");
+        strs.add("B");
+        strs.add("C");
+        strs.add("D");
+
+        System.out.println(strs);   //toString 지원됨
+
+        strs.add(1, "interupt");
+        System.out.println(strs);   //toString 지원됨
+
+        strs.removeFirst();
+        System.out.println(strs);   //toString 지원됨
+
+        /* list : 중복 허용, index 가 존재 */
+        for (int i=0; i<strs.size(); i++) {
+            String s = strs.get(i);
+            System.out.println(s);
+        }
+    }
+}
